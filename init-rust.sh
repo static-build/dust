7f04df442fc9a7f2a43c916255b23557f22fb517
edwinjhlee <edwin.jh.lee@gmail.com>
2021-07-24 22:08:12 +0800

    ...

--- init-rust.sh
+++
@@ -1,7 +0,0 @@
-. "$HOME/.cargo/env"
-
-rustup target add x86_64-apple-darwin
-rustup target add x86_64-apple-darwin
-rustup target add armv7-unknown-linux-musleabihf
-rustup target add x86_64-unknown-linux-musl
-rustup target add x86_64-pc-windows-gnu
